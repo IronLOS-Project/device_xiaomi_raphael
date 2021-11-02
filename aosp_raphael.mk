3a7f0a90a5adaa7b2d2fe7237b008a8031f425dd
ralf <ralf979@gmail.com>
2021-11-02 11:26:50 +0000

    Build Official
     - Xiaomi Gaming Mode
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -15,7 +15,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
+# Inherit some PEX stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+CUSTOM_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_raphael
